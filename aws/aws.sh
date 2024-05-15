d1939447fdb40008b72c5290c3893adc837c17ba
Mikołaj Woźniak <mw@shardlabs.io>
2024-05-15 14:34:07 +0200

    Update aws.sh
    [skip ci]

--- aws/aws.sh
+++ aws/aws.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-# TODO: test and use -> sudo usermod -a -G docker ec2-user
-
 sudo yum update -y
-
-# Amazon Linux 2023
-yum install -y docker
-
-# Amazon Linux 2
-# sudo amazon-linux-extras install docker
-
+yum install -y docker # Amazon Linux 2023
+# sudo amazon-linux-extras install docker # Amazon Linux 2
 sudo service docker start
 sudo docker pull shardlabs/starknet-devnet-rs
 sudo docker run -d -p "80:5050" shardlabs/starknet-devnet-rs:latest
